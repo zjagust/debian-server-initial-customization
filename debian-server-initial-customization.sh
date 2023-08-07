94c5098d55bf8cc6c9f5d9c3efe4346dc9b13003
Zeljko Jagust <zacks.adm@gmail.com>
2023-08-07 18:04:55 +0200

    Sed fix

--- debian-server-initial-customization.sh
+++ debian-server-initial-customization.sh
@@ -211,7 +211,7 @@ function sysdigRepo ()
 	curl -o /etc/apt/sources.list.d/draios.list https://download.sysdig.com/stable/deb/draios.list
 	if [[ "$OS_VERSION" -ge "12" ]]
 	then
-		sed -i "0,/deb/ s/deb/& [signed-by=\/usr\/share\/keyrings\/draios.gpg]/g" /etc/apt/sources.list.d/draios.list
+		sed -i "0,/deb/ s/deb/& [signed-by=\/usr\/share\/keyrings\/draios.gpg]/" /etc/apt/sources.list.d/draios.list
 	fi
 
 	# Update APT
